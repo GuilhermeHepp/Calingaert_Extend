f861fe1fc32a09cff0f6a21994cb210b76e82d37
Guilherme Hepp <131227740+GuilhermeHepp@users.noreply.github.com>
2025-07-18 22:54:10 -0300

    ultimos arquivos do montador

---
+++ Montador/TabelaSimbolos.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TabelaSimbolos {
+    
+private final Map<String, Integer> simbolos = new HashMap<>();
+
+    public void adicionar(String label, int endereco) {
+        simbolos.put(label, endereco);
+    }
+
+    public boolean contem(String label) {
+        return simbolos.containsKey(label);
+    }
+
+    public int getEndereco(String label) throws Exception {
+        if (!simbolos.containsKey(label)) throw new Exception("Símbolo não definido: " + label);
+        return simbolos.get(label);
+    }
+
+    int buscar(String op) {
+        if (simbolos.containsKey(op)) {
+            return simbolos.get(op);
+        } else {
+            throw new RuntimeException("Símbolo não definido: " + op);
+        }
+    }
+}
